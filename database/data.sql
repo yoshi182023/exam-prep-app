3c892a0d713a2fae7fffd147b0e5588c00aa82c2
zzse <125266206+yoshi182023@users.noreply.github.com>
2025-04-24 22:00:51 +0000

    My awesome feature

--- database/data.sql
+++ database/data.sql
@@ -1,11 +1,29 @@
--- Use SQL insert statements to add any
--- starting/dummy data to your database tables
 
--- EXAMPLE:
-
---  insert into "todos"
---    ("task", "isCompleted")
---    values
---      ('Learn to code', false),
---      ('Build projects', false),
---      ('Get a job', false);
+INSERT INTO questions ("questionid", topic, los, question, answer, explanation, a, b, c, "questionNumber")
+VALUES
+(
+    1,
+    'Ethical and Professional Standards',
+    'Global Investment Performance Standards (GIPS)',
+    'In cases where applicable local laws governing calculation and presentation of investment performance conflict with the GIPS standards, firms are:',
+    'a',
+    'In cases where applicable local laws governing calculation and presentation of investment performance conflict with the GIPS standards, firms must comply with local regulations and fully disclose the conflict in the compliant presentation.',
+    'required to comply with local regulations and make full disclosure of the conflict to claim GIPS compliance',
+    'unable to claim GIPS compliance in cases where local regulations prohibit accurate calculation',
+    'required to calculate and maintain two sets of performance data in order to claim GIPS compliance',
+    1
+);
+INSERT INTO questions (
+  questionid, topic, los, question, answer, explanation, a, b, c, "questionNumber"
+) VALUES (
+  2,
+  'Ethical and Professional Standards',
+  'Guidance for Standards I-VII, CFA Institute Standard III(B), Standard VI(B)',
+  'Ross Nelson, CFA, manages accounts for high-net-worth clients, including his own family''s account. He has no beneficial ownership in his family''s account. Because Nelson is concerned about the appearance of improper behavior in managing his family''s account, when his firm purchases a block of securities, Nelson allocates to his family''s account only those shares that remain after his other client accounts have their orders filled. The fee for managing his family''s account is based on his firm''s normal fee structure. According to the Standards of Practice Handbook, Nelson''s best course of action with regard to management of his family''s account would be to:',
+  'b',
+  'Nelson has breached his duty to his family by treating them differently from other clients. They are entitled to the same treatment as any other client of the firm. Nelson should treat his family''s account like any other client account as stated in Standard III (B) related to Fair Dealing and Standard VI (B) related to Priority of Transactions.',
+  'treat the account like other employee accounts of the firm.',
+  'treat the account like other client accounts.',
+  'remove himself from any direct involvement by transferring responsibility for this account to another investment professional in the firm.',
+  2
+);
