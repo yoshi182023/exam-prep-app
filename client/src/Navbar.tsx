812958360da3f465bb15372172be7a21031be6d9
zzse <125266206+yoshi182023@users.noreply.github.com>
2025-05-08 21:14:14 +0000

    css2

--- client/src/Navbar.tsx
+++ client/src/Navbar.tsx
@@ -2,9 +2,14 @@ import { NavLink } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <NavLink to="search">Search Questions</NavLink>
-    </div>
+    <>
+      <div className="navbar top">
+        <NavLink to="search">Search Questions</NavLink>
+      </div>{' '}
+      <div className="navbar bottom">
+        <NavLink to="wrong-answers">My Mistakes Journal</NavLink>
+      </div>
+    </>
   );
 }
 
