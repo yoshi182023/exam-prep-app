a54b5249acf435e7bfc426c3c5e0a9df4c631266
zzse <125266206+yoshi182023@users.noreply.github.com>
2025-05-08 18:01:33 +0000

    fix css

--- client/src/StripperWrapper.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/StripeWrapper.tsx
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY || '');
-
-export function StripeWrapper({ children }: { children: React.ReactNode }) {
-  return <Elements stripe={stripePromise}>{children}</Elements>;
-}
